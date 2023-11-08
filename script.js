e39031e698983d15cfb274d206cb9cef9a986ac2
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2023-11-08 20:27:02 +0100

    08-11-2023

---
+++ script.js
@@ -0,0 +1,95 @@
+const questions = [
+    {
+        questio: "Que pais tiene mas poblacion?",
+        respostaCorrecta: "China",
+        respostaIncorrecta: "India"
+    }, 
+
+    {
+        questio: "Quien fue el primer astronauta en aterrizar en la luna va a ser?",
+        respostaCorrecta: "Neil Amstrong",
+        respostaIncorrecta: "Louis Amstrong"
+    }
+];
+
+//
+let indexQuestioActual = 0;
+let respostesCorrectas = 0;
+let respostesIncorrectas = 0;
+
+const questioProposada = document.getElementById('questioProposada');
+const btnEsquerre = document.getElementById('btnEsquerre');
+const btnDret = document.getElementById('btnDret');
+const missatge = document.getElementById('missatge');
+const btnReiniciar = document.getElementById('btnReiniciar');
+
+//Mezcla las respuestas
+function barrejaRespostes(correcta, incorrecta){
+    const respostes = [correcta, incorrecta];
+    respostes.sort(() => {Math.random() - 0.5});
+    return respostes;
+}
+
+//
+function mostraQuestio(){
+
+    if(indexQuestioActual < questions.length){
+        const questioActual = questions[indexQuestioActual];
+        questioProposada.textContent = questioActual.questio;
+
+        const [barrejatCorrecta, barrejatIncorrecta] = barrejaRespostes(
+            questioActual.respostaCorrecta, 
+            questioActual.respostaIncorrecta
+        );
+
+        btnEsquerre.textContent = barrejatCorrecta;
+        btnDret.textContent = barrejatIncorrecta;
+
+    }else {
+        //Fuera del indice valido el juego a terminado
+
+        if(respostesCorrectas === questions.length){ //
+            missatge.textContent = "Has ganado, terminaste todas las preguntas";
+        }else {
+            missatge.textContent = `Juego acabado. Respuestas correctas :${respostesCorrectas}, Respuestas incorrectas: ${respostesIncorrectas}`;
+        }
+
+        btnEsquerre.style.display = "none";
+        btnDret.style.display = "none";
+        btnReiniciar.style.display = "block";
+    }
+}
+
+function comprovaResposta(respostaSeleccionada){
+    const questioActual = questions[indexQuestioActual];
+
+    if(respostaSeleccionada === questioActual){
+        respostesCorrectas++;
+    }else {
+        respostesIncorrectas++;
+    }
+
+    indexQuestioActual++;
+
+    mostraQuestio();
+}
+
+btnEsquerre.addEventListener("click", () => comprovaResposta(btnEsquerre.textContent));
+
+btnDret.addEventListener("click", () => comprovaResposta(btnDret.textContent));
+
+btnReiniciar.addEventListener("click", () => {
+    indexQuestioActual = 0;
+    respostesCorrectas = 0;
+    respostesIncorrectas = 0;
+
+    missatge.textContent = "";
+    btnEsquerre.style.display = "inline-block";
+    btnDret.style.display = "inline-block";
+    btnReiniciar.style.display = "none";
+
+    mostraQuestio();
+});
+
+//comenzar el juego 
+mostraQuestio();
